9f60feee2ba4f759fc23939e937de89def0320e9
Sanjay <39007646+sanjay7917@users.noreply.github.com>
2023-05-08 23:40:04 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "HELLO WRLDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD";
+        return "=====================THIS IS PRODUCTION TEST PIPELINE=====================";
     }
 
 }
